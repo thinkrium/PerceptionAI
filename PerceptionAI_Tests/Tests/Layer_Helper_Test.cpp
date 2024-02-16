3332a607cf9357369a152c16ac9ea58d64d10f99
Thom Simmons <thomedy@thinkrium.com>
2024-02-16 06:02:07 -0600

    added some tests

--- PerceptionAI_Tests/Tests/Layer_Helper_Test.cpp
+++ PerceptionAI_Tests/Tests/Layer_Helper_Test.cpp
@@ -7,7 +7,7 @@
 
 using namespace Perception::Network::Utilities;
 // Demonstrate some basic assertions.
-TEST(Layer_Helper_Test, Prepare_Forward_Propagation_Test) {
+TEST(Layer_Helper_Test, Prepare_Forward_Propagation_Test_3_Nodes) {
 
 
 
@@ -61,12 +61,110 @@ TEST(Layer_Helper_Test, Prepare_Forward_Propagation_Test) {
     currentLayer.setWeights(currentLayersWeightsMatrix);
 
     // assert
+    Perception_Element_Vector<Node> expectedValue;
 
+    vector<Node> values = {Node(14),Node(14),Node(14)};
+
+    expectedValue.setElementVector(values);
     // test
 
     layerHelper.Prepare_Forward_Propagation_Without_Activating(previousLayer, currentLayer);
 
-    EXPECT_EQ(1,1);
+    Perception_Element_Vector<Node> actualValue = currentLayer.getLocalNodes();
+    bool valuesAreEqual = true;
+
+    for(int i = 0; i < currentLayersNodeCount; i++) {
+        if (
+                expectedValue.getElementAt(i).getValue()
+                !=
+                actualValue.getElementAt(i).getValue()
+                ) {
+            valuesAreEqual = false;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+
+    EXPECT_TRUE(valuesAreEqual);
+}
+
+TEST(Layer_Helper_Test, Prepare_Forward_Propagation_Test_4_Nodes) {
+
+
+
 ///////////////////////////////////////////////////////////////////
 
+    Layer_Helper layerHelper;
+
+
+    int previousLayerNodeCount = 4;
+    int currentLayersNodeCount = 4;
+
+    Layer previousLayer(previousLayerNodeCount), currentLayer(currentLayersNodeCount);
+
+    Perception_Element_Vector<Node> previousOutputsNodeVector;
+
+    Perception_Element_Matrix<Weight> currentLayersWeightsMatrix;
+
+    vector<Node> previousOutputNodes (previousLayerNodeCount);
+
+    vector<vector<Weight>> currentLayersWeights(previousLayerNodeCount, vector<Weight>(currentLayersNodeCount));
+
+    // set output nodes
+    for(int i = 0; i < previousLayerNodeCount; i++) {
+        Node node;
+        node.setValue((float)(i + 1));
+
+        previousOutputNodes.at(i) = node;
+    }
+
+    // set weights
+    for(int i = 0; i < currentLayersNodeCount; i++) {
+        for(int j = 0; j < currentLayersNodeCount; j++) {
+            Weight weight;
+
+            weight.setValue((float)(j + 1));
+
+            currentLayersWeights[i][j] = weight;
+        }
+    }
+
+    previousOutputsNodeVector.setElementVector(previousOutputNodes);
+
+    currentLayersWeightsMatrix.setElementMatrix(currentLayersWeights);
+
+    previousOutputsNodeVector.setHealthStatus(Enumerations::Perception_Enumerations::healthStatus::ok);
+
+    currentLayersWeightsMatrix.setHealthStatus(Enumerations::Perception_Enumerations::healthStatus::ok);
+
+    previousLayer.setOutputNodes(previousOutputsNodeVector);
+
+    currentLayer.setWeights(currentLayersWeightsMatrix);
+
+    // assert
+    Perception_Element_Vector<Node> expectedValue;
+
+    vector<Node> values = {Node(30),Node(30),Node(30),Node(30)};
+
+    expectedValue.setElementVector(values);
+    // test
+
+    layerHelper.Prepare_Forward_Propagation_Without_Activating(previousLayer, currentLayer);
+
+    Perception_Element_Vector<Node> actualValue = currentLayer.getLocalNodes();
+    bool valuesAreEqual = true;
+
+    for(int i = 0; i < currentLayersNodeCount; i++) {
+        if (
+                expectedValue.getElementAt(i).getValue()
+                !=
+                actualValue.getElementAt(i).getValue()
+                ) {
+            valuesAreEqual = false;
+        }
+    }
+
+    ///////////////////////////////////////////////////////////////////
+
+    EXPECT_TRUE(valuesAreEqual);
 }
